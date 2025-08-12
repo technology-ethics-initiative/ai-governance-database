a0f4ab6c4de0aca20ea05974eb5df9ff8a2cf366
ARena18 <91445893+ARena18@users.noreply.github.com>
2025-08-12 09:45:24 -0700

    add navbars and home

---
+++ src/Components/DataTable.tsx
@@ -0,0 +1,19 @@
+import { GitHubIcon, LaunchIcon } from "./Icons";
+import styles from "./DataTable.module.css";
+
+interface DataTableProps {
+
+}
+
+function DataTable(table: DataTableProps) {
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export type { DataTableProps };
+export default DataTable;

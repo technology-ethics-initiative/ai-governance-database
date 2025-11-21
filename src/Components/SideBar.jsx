e9ee6ba25983a9b735853071948693a4ca782a73
ahnrena.18@gmail.com <ahnrena.18@gmail.com>
2025-11-21 05:02:38 -0800

    modifies import of data for dropdown pages

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { AboutIcon, AlbumIcon, BookIcon, CheckIcon, ListIcon, MinusIcon, PersonIcon, PlusIcon } from "./Icons";
 import styles from './SideBar.module.css';
+import { pagesData } from "../data/news";
+
+console.log(Object.keys(pagesData.laws))
+const countries = Object.keys(pagesData.laws);
+const companies = Object.keys(pagesData.lawsuits);
 
 function SideBar(props) {
+  const [lawsDrop, setLawsDrop] = useState(false);
+  const [suitsDrop, setSuitsDrop] = useState(false);
+
   return (
     <>
       {props.open ? (
@@ -20,17 +28,43 @@ function SideBar(props) {
             </button>
           </a>
           <a href="#/laws-and-policies">
-            <button>
+            <button id="lawsDropdown" onClick={() => setLawsDrop(!lawsDrop)}>
               <AlbumIcon height="1.2em" width="1.2em" />
               Laws and Policies
+              { lawsDrop ? (
+                  <MinusIcon height="1.2em" width="1.2em" />
+                ) : (
+                  <PlusIcon height="1.2em" width="1.2em" />
+                )
+              }
             </button>
           </a>
+          <div className={styles.dropDown + " " + (lawsDrop ? styles.open : "")}>
+            {countries.map((country) => (
+              <a key={country} href={"#/laws-and-policies/" + country}>
+                <button>{country}</button>
+              </a>
+            ))}
+          </div>
           <a href="#/lawsuits">
-            <button>
+            <button id="suitsDropdown" onClick={() => setSuitsDrop(!suitsDrop)}>
               <ListIcon height="1.2em" width="1.2em" />
               Lawsuits
+              { suitsDrop ? (
+                  <MinusIcon height="1.2em" width="1.2em" />
+                ) : (
+                  <PlusIcon height="1.2em" width="1.2em" />
+                )
+              }
             </button>
           </a>
+          <div className={styles.dropDown + " " + (suitsDrop ? styles.open : "")}>
+            {companies.map((company) => (
+              <a key={company} href={"#/lawsuits/" + company}>
+                <button>{company}</button>
+              </a>
+            ))}
+          </div>
           <a href="#/business-self-regulation">
             <button>
               <PersonIcon height="1.2em" width="1.2em" />
